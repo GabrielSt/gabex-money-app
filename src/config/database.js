9d52c7d96b9c27afffb9bf0abec88fca3af4ed54
DIR\g.dos.santos.pereira <gabriel.pereira@concretesolutions.com.br>
2018-09-17 13:52:21 -0300

    teste na url do mongo

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,23 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney'
-module.exports = mongoose.connect(encodeURI(url), { useNewUrlParser: true })
+//module.exports = mongoose.connect(encodeURI(url), { useNewUrlParser: true })
+console.log(url)
+module.exports = mongoose.connect('mongodb://@ds155862.mlab.com:55862/heroku_1vbrm3fb', {
+    auth: {
+      user: "mymoneyapp",
+      password: "Qwe@96845255"
+    },
+    useNewUrlParser:true
+      }).then(
+        () => { 
+            console.log("Database connected");
+        },
+        err => { 
+            /** handle initial connection error */ 
+            console.log("Error in database connection. ", err);
+        }
+    );
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min =
